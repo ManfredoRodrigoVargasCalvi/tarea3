c64f38f4573f63cd97ae6741cfb82bfeed477a79
ManfredoRodrigoVargasCalvi <81886110+ManfredoRodrigoVargasCalvi@users.noreply.github.com>
2021-06-27 20:38:43 -0400

    Add files via upload

---
+++ nombreyaperllido.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class nombreyaperllido
+{
+   public static void main (String [] args){
+       Scanner add = new Scanner (System.in);
+       
+       System.out.println("Escribe tu nombre");
+       
+       String primero = add.next(); 
+       
+       System.out.println("Escribe tu apellido");
+       
+       String segundo = add.next(); 
+       
+       System.out.println("Mi nombre es " + primero);
+       System.out.println("Mi apellido es " + segundo);
+   }
+    
+    
+    
+    
+    
+    
+    
+}  
